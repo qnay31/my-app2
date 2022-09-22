e180de65fabd35a9bb7782dcda1697434f64e3a9
qnay31 <rizkyamien@gmail.com>
2022-09-22 14:05:56 +0700

    upate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoading: true,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          items: data,
+          isLoading: false,
+        })
+      );
+  }
+
+  render() {
+    const { items, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p> Loading........ </p>;
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ol>
+            {" "}
+            {items.map((item, index) => (
+              <li key={index}>
+                Nama: {item.name} <br />
+                Username: {item.username} <br />
+                Alamat: {item.address.street}{" "}
+              </li>
+            ))}{" "}
+          </ol>{" "}
+        </header>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
